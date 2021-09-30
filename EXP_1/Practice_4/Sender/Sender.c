9586331fd613f667a99466c2b44063c49e9942eb
MehrabToghani <MehrabToghani@Gmail.com>
2021-09-30 15:08:59 +0000

    [Add] LCD.

--- EXP_1/Practice_4/Sender/Sender.c
+++ EXP_1/Practice_4/Sender/Sender.c
@@ -3,26 +3,29 @@
 #include <string.h>
 #include <util/delay.h>
 
-#define START_BYTE 0x31
-
 int main()
 {
-    char message[] = "Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message, Good Bye!";
+    char message[] = "Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message, Good Bye!!";
 
     DDRC = 0xFF; // set PORT C for output
+    DDRD = DDRD | 0x01; // set first bit of PORT D for output
+
+    PORTC = 0; // init PORT C
+    PORTD = 0; // init PORT D
 
     eeprom_busy_wait();                              // wait to eeprom be free
     eeprom_write_block(message, 0, strlen(message)); // write message to eeprom
 
-    // start messaging
-    PORTC = START_BYTE;
-    _delay_ms(10);
-
     for (int i = 0; i < strlen(message); i++)
     {
-        PORTC = eeprom_read_byte((const uint8_t*) i);
+        PORTC = message[i];
+        PORTD = 0x01;
+        _delay_ms(10);
+        PORTD = 0;
         _delay_ms(10);
     }
+
+    PORTC = 0;
 
     return 0;
 }
