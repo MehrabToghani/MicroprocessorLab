6abaf9766be7c93fe27b692756c3c13d35f67037
MehrabToghani <MehrabToghani@Gmail.com>
2021-09-30 17:22:35 +0000

    [Add] LCD.

---
+++ EXP_1/Practice_5/README.md
@@ -0,0 +1,8 @@
+## Reference
+
+this is reference of LCD codes:
+https://www.electronicwings.com/avr-atmega/lcd16x2-interfacing-with-atmega16-32
+
+## Sample
+
+![alt text](sample.gif "sample.gif")
--- EXP_1/Practice_5/Sender/Sender.c
+++ EXP_1/Practice_5/Sender/Sender.c
@@ -3,40 +3,39 @@
 #include <string.h>
 #include <util/delay.h>
 
-#define START_BYTE 0x01
-
-// set specific bit to 1
-int BV(int bit)
-{
-    return 1 << bit;
-}
-
 int main()
 {
-    char message[] = "Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message, Good Bye!";
+    char message[] = "Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message From Atmega16 Microcontroller, Test Message, Good Bye!!";
 
-    DDRC = 0xFF; // set PORT C for output
+    DDRC = DDRC | 0x01; // set first bit of PORT C for output
+    DDRD = DDRD | 0x01; // set first bit of PORT D for output
+
+    PORTC = 0; // init PORT C
+    PORTD = 0; // init PORT D
 
     eeprom_busy_wait();                              // wait to eeprom be free
     eeprom_write_block(message, 0, strlen(message)); // write message to eeprom
 
-    // start messaging
-    PORTC = START_BYTE;
-    _delay_ms(10);
-
     for (int i = 0; i < strlen(message); i++)
     {
-        char data = eeprom_read_byte((const uint8_t *)i);
-
         for (int j = 0; j < 8; j++)
         {
-            if (data & BV(j))
-                PORTC = PORTC ^ BV(j);
+            if ((message[i] & (1 << j)) != 0)
+            {
+                PORTC = 0x01;
+            }
             else
-                PORTC = PORTC & ~BV(j);
-            _delay_ms(10);
+            {
+                PORTC = 0;
+            }
+            PORTD = 0x01;
+            _delay_ms(2);
+            PORTD = 0;
+            _delay_ms(2);
         }
     }
+
+    PORTC = 0;
 
     return 0;
 }
